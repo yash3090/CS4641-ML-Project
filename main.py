a1dbfef6b54e74325be9791f92eb08f0bf06f060
Matthew Landis <matthewlandis5@gmail.com>
2023-10-05 16:11:54 -0400

    Fix typos.

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ st.write('By Project Group 13: Ashley Cain, Ethan Haarer, Keshav Jagannath, Matt
 #   A quick introduction of your topic and mostly literature review of what has been done in this area. 
 #   You can briefly explain your dataset and its features here too.
 st.header('Introduction')
-introduction = 'The goal of this project is to identify the instrument being played from a given snippet of audio. This concept has been explored in several research articles as part of bigger audio projects, such as clustering of instrument catagories from a multi-instrument recording. We will be using an established dataset of audio files such as NSynth to train a convolutional neural network to fulfill this goal. This dataset also contains classifiers such as "acoustic", "synthetic", and "electronic", as well as "attack" and "decay" time. These are all useful features to have when describing a snippet of audio. NSynth already has its audio files encoded down to integer arrays, and comes with a wide featureset that we will be able to use for training.'
+introduction = 'The goal of this project is to identify the instrument being played from a given snippet of audio. This concept has been explored in several research articles as part of bigger audio projects, such as clustering of instrument categories from a multi-instrument recording. We will be using an established dataset of audio files such as NSynth to train a convolutional neural network to fulfill this goal. This dataset also contains classifiers such as "acoustic", "synthetic", and "electronic", as well as "attack" and "decay" time. These are all useful features to have when describing a snippet of audio. NSynth already has its audio files encoded down to integer arrays, and comes with a wide featureset that we will be able to use for training.'
 st.write(introduction)
 
 # Problem definition: 
 #   Why there is a problem here or what is the motivation of the project?
 st.header('Problem Definition')
-problem_definition = 'AI classification in the audio realm is a relatively new research field compared to applications such as computer vision. Single instrument classification is an intermediary towards a host of other applications. Companies like "Izotope" are currently interested in developing mixing and mastering virtual assistants based on the ability to classify waveforms into buckets of instrument catagories. It would also be useful for individuals to classify instrument details from rehearsal recordings, auditions, or performances for further review.'
+problem_definition = 'AI classification in the audio realm is a relatively new research field compared to applications such as computer vision. Single instrument classification is an intermediary towards a host of other applications. Companies like "Izotope" are currently interested in developing mixing and mastering virtual assistants based on the ability to classify waveforms into buckets of instrument categories. It would also be useful for individuals to classify instrument details from rehearsal recordings, auditions, or performances for further review.'
 st.write(problem_definition)
 
 # Methods: 
@@ -23,14 +23,16 @@ st.write(problem_definition)
 #   Students are encouraged to use existing packages and libraries (i.e. scikit-learn) instead of coding the algorithms from scratch.
 st.header('Methods')
 methods = """
-We will be using the NSynth library and its included feature set in order to train a convolutional neural network. We decided upon a CNNs by reading different research papers that extract the instruments used from polyphonic audio files and found this to be the most efficient way for our application. 
-This particular library has encoded its audio files to the format required.
-For the ML framework, we will be using PyTorch for our multi-class classification model.
-We are beginning with the creation of a supervised model which will use the ground truths to train the convolutional neural network.
-If we make signficant progress on the supervised model, we will create an unsupervised model which will use extracted features to define clusters, optimized via a silloutte coefficient.
-Data visualization will be done using the matplotlib library and the built-in functionality of Streamlit.
-Our model will be trained on the NSynth dataset containing over 300,000 samples and organized around the instrument being played.
-We will first preprocess the data we have from NSynth for our CNN. We will then create the model. After model creation we will train the model and test it on the test set. Using the information from test set results we will tune our hyperparmaters and extract the most informative features. The last two steps will be repeated to fine tune our model and achieve higher accuracies.
+We will be using the NSynth library and its included feature set in order to train a convolutional neural network. 
+We decided upon CNNs by reading different research papers that extract the instruments used from polyphonic audio files and found this to be the most efficient way for our application. 
+This particular library has encoded its audio files to the format required. 
+For the ML framework, we will be using PyTorch for our multi-class classification model. 
+We are beginning with the creation of a supervised model which will use the ground truths to train the convolutional neural network. 
+If we make significant progress on the supervised model, we will create an unsupervised model which will use extracted features to define clusters, optimized via a silhouette coefficient. 
+Data visualization will be done using the matplotlib library and the built-in functionality of Streamlit. Our model will be trained on the NSynth dataset containing over 300,000 samples and organized around the instrument being played. 
+We will first preprocess the data we have from NSynth for our CNN. We will then create the model. After model creation we will train the model and test it on the test set. 
+Using the information from test set results we will tune our hyperparameters and extract the most informative features. 
+The last two steps will be repeated to fine tune our model and achieve higher accuracies.
 """
 #Librosa: https://librosa.org/
 #PyTorch: https://www.learnpytorch.io/02_pytorch_classification/
@@ -42,8 +44,8 @@ st.write(methods)
 st.header('Potential Results and Discussion')
 potential_results_and_discussion = """
 We will be looking for the success rate for which the model can identify the instrument being played in a given audio file. 
-This testing will be done on a reserved portion of the dataset, and we are hoping to achieve a success rate of 95% or higher.
-When applying the model to real world scenarios, we will be looking on how the model can extract the music from any background noise.
+This testing will be done on a reserved portion of the dataset, and we are hoping to achieve a success rate of 95% or higher. 
+When applying the model to real world scenarios, we will be looking at how the model can extract the music from any background noise.
 """
 st.write(potential_results_and_discussion)
 
